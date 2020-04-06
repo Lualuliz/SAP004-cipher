efabb4242ca39f4261aba027f513915ac02baa64
Lualuliz <luana.las21@gmail.com>
2020-04-06 12:05:33 -0300

    Pagina html,css praticamente prontas, trabalhando no encode do cipher.js e no addEventListener no index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
-const cipher = {
+const cipher {
   // ...
-};
+  function Cesar (offset, string) {
+    let result = '';
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWYXZ';
+    
+    for (let i = 0; i < string.length; i += 1) {
+        let char = string[i].toUpperCase();
+        let alphabetIndex = alphabet.indexOf(char);
+        let newChar = alphabet[(alphabetIndex + offset) % alphabet.length];
+      
+      if (char === ' ') {
+        result += ' ';
+      } else {
+        result += newChar;
+      }
+    }
+    
+    return result;
+  }
+}
+
+ 
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
+let button1 = document.getElementById("button1");
+
+
+function text() {
+  let firstText = document.getElementById("box1").value;
+  let secondText = document.getElementById("box2");
+  
+  secondText.value = firstText;
+}
+firstText = result;
+
+button1.addEventListener("click",text);
+
+
 console.log(cipher);
