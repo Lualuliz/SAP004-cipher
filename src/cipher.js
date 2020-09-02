882f9c61707f6a7f500e2fd7241e8d7009b7d673
luanaandrade <57915141+Lualuliz@users.noreply.github.com>
2020-09-02 12:56:33 -0300

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,75 +1,114 @@
 const cipher = {
-  encode:(offset, string) => {
-    if (typeof(offset) != typeof(0)){
-      throw new TypeError()
+  encode: (offset, string) => {
+    if (typeof offset != typeof 0) {
+      throw new TypeError();
+    } else if (typeof string != typeof "") {
+      throw new TypeError();
     }
-    else if(typeof(string)!= typeof("")) {
-      throw new TypeError()
-     
-    }
-   
-    let result = ''
-    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z']
-    
-   
+
+    let result = "";
+    let alphabet = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
 
     for (let i = 0; i < string.length; i++) {
-        let char = string[i].toUpperCase()
-        let alphabetIndex = alphabet.indexOf(char)
-        let newChar = alphabet[(alphabetIndex + offset) % alphabet.length]
+      let char = string[i].toUpperCase();
+      let alphabetIndex = alphabet.indexOf(char);
+      let newChar = alphabet[(alphabetIndex + offset) % alphabet.length];
 
-       
-
-      if (char === ' ') {
-                                        
-        result += ' '
-        
-
+      if (char === " ") {
+         result += " ";
       } else {
-        result += newChar
-      }
-
-     
-      
-    }
-
-    return result
-  
-},
-
-decode:(offset,string) => {
-  if (typeof(offset) != typeof(0)){
-    throw new TypeError()
-  }
-  else if(typeof(string)!= typeof("")) {
-    throw new TypeError()
-   
-  }
-  let result = ''
-    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W', 'X','Y','Z']
-    
-    for (let i = 0; i < string.length; i++) {
-        let char = string[i].toUpperCase()
-        let alphabetIndex = alphabet.indexOf(char)
-        
-        let newChar = alphabet[((alphabetIndex + alphabet.length + alphabet.length) - offset) % alphabet.length ]
-        
-        
-      
-      if (char === ' ') {
-        result += ' '
-      } else {
-        result += newChar
+       result += newChar;
       }
     }
 
-    return result
-  
-
+    return result;
   },
-}
 
-export default cipher
+  decode: (offset, string) => {
+    if (typeof offset != typeof 0) {
+      throw new TypeError();
+    } else if (typeof string != typeof "") {
+      throw new TypeError();
+    }
+    let result = "";
+    let alphabet = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+
+    for (let i = 0; i < string.length; i++) {
+      let char = string[i].toUpperCase();
+      let alphabetIndex = alphabet.indexOf(char);
+
+      let newChar =
+        alphabet[
+          (alphabetIndex + alphabet.length + alphabet.length - offset) %
+            alphabet.length
+        ];
+
+      if (char === " ") {
+        result += " ";
+      } else {
+        result += newChar;
+      }
+    }
+
+    return result;
+  },
+};
+
+export default cipher;
 
 
 
