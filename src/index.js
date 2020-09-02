92a9735cbb187b3b203761f64b587acae583fdbd
luanaandrade <57915141+Lualuliz@users.noreply.github.com>
2020-09-02 13:08:10 -0300

    Update index.js
    hotfix em ids

--- src/index.js
+++ src/index.js
@@ -1,49 +1,39 @@
-import cipher from './cipher.js'
+import cipher from "./cipher.js";
 
-let button1 = document.getElementById("button1")
+let button1 = document.getElementById("button1");
 
 function text() {
-  
+  let firstText = document.getElementById("box1").value;
 
-  let firstText = document.getElementById("box1").value
-  
-  let key = document.getElementById('roll').valueAsNumber
+  let key = document.getElementById("roll").valueAsNumber;
 
-  let secondText = document.getElementById("box2")
+  let secondText = document.getElementById("box2");
 
-  let message = secondText.value = firstText
+  let message = (secondText.value = firstText);
 
+  let encodeMessage = cipher.encode(key, message);
 
-  let encodeMessage = cipher.encode(key,message)
-
- secondText.value = encodeMessage
-
+  secondText.value = encodeMessage;
 }
 
-let button2 = document.getElementById("button2")
+let button2 = document.getElementById("button2");
 
-function text2(){
-
-  let firstText = document.getElementById("box1").value
+function text2() {
   
-  let key = document.getElementById('roll').valueAsNumber
+  let firstText = document.getElementById("box1").value;
 
-  let secondText = document.getElementById("box2")
+  let key = document.getElementById("roll").valueAsNumber;
 
-  let message = secondText.value = firstText
+  let secondText = document.getElementById("box2");
 
+  let message = (secondText.value = firstText);
 
-  let decodeMessage = cipher.decode(key,message)
+  let decodeMessage = cipher.decode(key, message);
 
- secondText.value = decodeMessage
-
-
+  secondText.value = decodeMessage;
 }
 
+button1.addEventListener("click", text);
+button2.addEventListener("click", text2);
 
-button1.addEventListener("click",text)
-button2.addEventListener("click",text2)
-
-
-
-console.log(cipher)
+console.log(cipher);
